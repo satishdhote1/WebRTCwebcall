d1f52fddec3c42fac19977cd3dec6bbe9ca14803
Altanai <tara181989@gmail.com>
2016-07-14 19:38:38 +0530

    added property

---
+++ property.js
@@ -0,0 +1,14 @@
+props={
+	extensionID: "elfbfompfpakbefoaicaeoabnnoihoac",
+	serverNAME: "WebRTCDevelopemnet",
+	env: "local",
+	signaller: "https://"+location.hostname+":8084/",
+    xirsys:{
+        username:"satishdhote",
+        secretkey:"32562542-49c5-11e6-8633-2eff9bb1bbe1",
+        domain:"www.villageexperts.com",
+        application:"webrtc",
+        room:"service",
+        secure:1
+    }
+}
