08eb22c5b71d32b7e884aa47a626e1849c69772a
Altanai <tara181989@gmail.com>
2017-03-22 21:50:16 +0530

    adjust element

---
+++ js/ve.js
@@ -0,0 +1,14 @@
+function adjustelement(){
+	/*$("#mainDiv").height(window.innerHeight- $(".header-part").height())*/
+	$("#mainDiv").height(window.innerHeight);
+	$("#leftDiv").height($("#mainDiv").height()-20);
+	$("#rightDiv").height($("#mainDiv").height()-20);
+	$("#centreDiv").height($("#mainDiv").height()-16);
+	$(".fileviewing-box").css("height",$("#mainDiv").height()-150);
+	$(".filesharing-box").css("height","130px");
+	$('[data-toggle="tooltip"]').tooltip();
+}
+
+document.getElementById("logoutBtn").onclick=function(){
+  window.location="https://www.villageexperts.com/";
+}
