fd219bf3f411577b11a2ef1d172d472452605a5a
Altanai <tara181989@gmail.com>
2017-05-19 21:40:22 +0530

    conf popup videos

--- js/ve.js
+++ js/ve.js
@@ -13,26 +13,3 @@ document.getElementById("logoutBtn").onclick=function(){
   //window.location="https://"+window.location.hostname+"/";
   window.location="https://"+window.location.hostname+"/connect.php?endTime="+sessionid;
 }
-
-function onVideoAdded() {
-    var allvids = document.getElementsByTagName("video");
-    var count;
-    for (x in allvids) {
-        if (document.getElementsByName(x) && document.getElementsByName(x).length>0 && document.getElementsByName(x)[0].src) 
-            count++;
-    }
-
-    alert(" on video added");
-    if (count > 3) {
-        document.getElementsByClassName("leftDiv1").hidden = false;
-    }
-    if (count > 5) {
-        document.getElementsByClassName("leftDiv2").hidden = false;
-    }
-}
-
-var allvids = document.getElementsByTagName("video");
-for (x in allvids) {
-    if (document.getElementsByName(x) && document.getElementsByName(x).length>0 && document.getElementsByName(x)[0].src) 
-        document.getElementsByName(x)[0].addEventListener('onplaying', onVideoAdded, false);
-}
