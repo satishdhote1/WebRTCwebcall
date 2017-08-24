2a3afbe5b23a3811074a9f83c50e864e31d7df05
altanai <tara181989@gmail.com>
2017-08-24 11:34:23 +0530

    index support

--- property.js
+++ property.js
@@ -1,14 +1,16 @@
-props={
-	extensionID: "jpcjjkpbiepbmhklnjoahacppaemhmpd",
-	serverNAME: "WebRTCDevelopemnet",
-	env: "local",
-	signaller: "https://"+location.hostname+":8084/",
-    xirsys:{
-        username:"farookafsari",
-        secretkey:"f2b0baba-eff5-11e6-910e-5a6c29453559",
-        domain:"www.villageexperts.com",
-        application:"villageexpertsproduction",
-        room:"friendsandfamily",
-        secure:1
+props = {
+    extensionID: "gjmoghcimpghddpghephlknpiapnljbn",
+    serverNAME: "WebRTCDevelopemnet",
+    env: "local",
+    signaller: "https://" + location.hostname + ":8084/",
+    xirsys: {
+        username: "farookafsari",
+        secretkey: "f2b0baba-eff5-11e6-910e-5a6c29453559",
+        domain: "www.villageexperts.com",
+        application: "villageexpertsproduction",
+        room: "friendsandfamily",
+        secure: 1
     }
-}
+};
+
+//gjmoghcimpghddpghephlknpiapnljbn;
