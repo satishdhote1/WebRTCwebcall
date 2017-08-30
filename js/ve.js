52dc23b572f6ead1c74d2d17097e4689cfdd8a2d
altanai <tara181989@gmail.com>
2017-08-30 10:05:25 +0530

    height of central div

--- js/ve.js
+++ js/ve.js
@@ -3,8 +3,8 @@ function adjustelement(){
 	$("#mainDiv").height(window.innerHeight);
 	$("#leftDiv").height($("#mainDiv").height()-20);
 	$("#rightDiv").height($("#mainDiv").height()-20);
-	$("#centreDiv").height($("#mainDiv").height());
-	//$("#centreDiv").height("100%");
+	//$("#centreDiv").height($("#mainDiv").height());
+	$("#centreDiv").height("100%");
 	$(".fileviewing-box").css("height",$("#mainDiv").height()-150);
 	$(".filesharing-box").css("height","130px");
 	$('[data-toggle="tooltip"]').tooltip();
