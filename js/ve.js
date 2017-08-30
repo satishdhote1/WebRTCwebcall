c4d5dd1a75dd31d85b5e3db0c89e1346896edf97
altanai <tara181989@gmail.com>
2017-08-30 10:41:26 +0530

    auto :

--- js/ve.js
+++ js/ve.js
@@ -5,8 +5,8 @@ function adjustelement(){
 	$("#rightDiv").height($("#mainDiv").height()-20);
 	//$("#centreDiv").height($("#mainDiv").height());
     $("#centreDiv").height("100%");
-    $("fileSharingRow").height("50%");
-    $("fileListingRow").height("50%");
+    $("#fileSharingRow").height("50%");
+    $("#fileListingRow").height("50%");
 
 	$(".fileviewing-box").css("height","100%");
     $(".filesharing-box").css("height","100%");
