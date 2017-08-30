9a3d0035c888b4e94dc4d6c7fc8c2e6c2ee88d74
altanai <tara181989@gmail.com>
2017-08-30 10:38:43 +0530

    auto :

--- js/ve.js
+++ js/ve.js
@@ -4,9 +4,12 @@ function adjustelement(){
 	$("#leftDiv").height($("#mainDiv").height()-20);
 	$("#rightDiv").height($("#mainDiv").height()-20);
 	//$("#centreDiv").height($("#mainDiv").height());
-	$("#centreDiv").height("100%");
-	$(".fileviewing-box").css("height",$("#mainDiv").height()-150);
-	$(".filesharing-box").css("height","130px");
+    $("#centreDiv").height("100%");
+    $("fileSharingRow").height("50%");
+    $("fileListingRow").height("50%");
+
+	$(".fileviewing-box").css("height","100%");
+    $(".filesharing-box").css("height","100%");
 	$('[data-toggle="tooltip"]').tooltip();
 }
 
